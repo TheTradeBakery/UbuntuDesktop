2dfe4ac8cc532c607a3337739bf435bc788aca99
The Trade Bakery <thetradebakery@gmail.com>
2023-04-20 16:46:21 -0400

    updated readme.md and run script

--- ansible_install.sh
+++ ansible_install.sh
@@ -3,6 +3,6 @@ sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 ansible-galaxy install jaredhocutt.gnome_extensions
-wget https://raw.githubusercontent.com/TheTradeBakery/UbuntuDesktop/e0eede59e9dadff7b031df2a91a8632ca74f3262/ubuntu_setup.yaml
+wget https://raw.githubusercontent.com/TheTradeBakery/UbuntuDesktop/main/ubuntu_setup.yaml
 ansible-playbook --ask-become-pass ubuntu_setup.yaml
 rm ubuntu_setup.yaml
