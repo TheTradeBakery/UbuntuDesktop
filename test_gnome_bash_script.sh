a2f8ba4aed9b7475bed94d97c6ce84db57804490
The Trade Bakery <thetradebakery@gmail.com>
2023-04-21 13:22:30 -0400

    updated run script

--- test_gnome_bash_script.sh
+++ test_gnome_bash_script.sh
@@ -10,8 +10,8 @@ download_url="https://extensions.gnome.org${download_url_extension}"
 echo $pretty_name 
 wget -q -O ${uuid}.zip $download_url
     if ! gnome-extensions list | grep --quiet ${uuid}; then
-    echo $USER
-    busctl --machine=$USER@.host --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${uuid}
+    localuser=$(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1)
+    busctl --machine=$localuser@.host --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${uuid}
 fi
 gnome-extensions enable ${uuid}
 rm ${uuid}.zip
