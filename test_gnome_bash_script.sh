0cdd1e5d9a6308ebcde7995c7a278c471daa9a28
The Trade Bakery <thetradebakery@gmail.com>
2023-04-24 09:19:55 -0400

    updated run script

--- test_gnome_bash_script.sh
+++ test_gnome_bash_script.sh
@@ -8,11 +8,11 @@ uuid=$(echo $extension_info | grep -o -P '(?<="uuid": ").*(?=", "name")')
 pretty_name=$(echo $extension_info | grep -o -P '(?<="name": ").*(?=", "creator")')
 download_url="https://extensions.gnome.org${download_url_extension}"
 echo $pretty_name 
-if ! gnome-extensions list | grep --quiet ${uuid}; then
+if ! ls ~/.local/share/gnome-shell/extensions/ | grep --quiet ${uuid}; then
     wget -q -O ${uuid}.zip $download_url
     localuser=$(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1)
     busctl --user --machine=$localuser@.host call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${uuid}
-    gnome-extensions enable ${uuid}
+    # gnome-extensions enable ${uuid} # will not work as root therefore commented out
     rm ${uuid}.zip
 fi
 
