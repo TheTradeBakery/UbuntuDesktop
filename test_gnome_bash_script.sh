014f44128d74b5dc1e9f32ad3dc9806c0021791b
The Trade Bakery <thetradebakery@gmail.com>
2023-04-23 22:34:51 -0400

    updated run script

--- test_gnome_bash_script.sh
+++ test_gnome_bash_script.sh
@@ -8,10 +8,11 @@ uuid=$(echo $extension_info | grep -o -P '(?<="uuid": ").*(?=", "name")')
 pretty_name=$(echo $extension_info | grep -o -P '(?<="name": ").*(?=", "creator")')
 download_url="https://extensions.gnome.org${download_url_extension}"
 echo $pretty_name 
-wget -q -O ${uuid}.zip $download_url
-    if ! gnome-extensions list | grep --quiet ${uuid}; then
+if ! gnome-extensions list | grep --quiet ${uuid}; then
+    wget -q -O ${uuid}.zip $download_url
     localuser=$(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1)
     busctl --user --machine=$localuser@.host call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${uuid}
+    gnome-extensions enable ${uuid}
+    rm ${uuid}.zip
 fi
-gnome-extensions enable ${uuid}
-rm ${uuid}.zip
+
